597826bf36a0e99baf5ea0e82cf7c63b02f55903
Dice-K39 <daisukehashimoto39@gmail.com>
2020-08-31 19:12:41 -0400

    Created Property model, controller, and views.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class PropertiesController < ApplicationController
+  def index
+    @properties = Property.all
+  end
+
   def new
     @property = Property.new
   end
@@ -12,6 +16,32 @@ class PropertiesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @property = Property.find_by_id(params[:id])
+  end
+
+  def update 
+    @property = Property.find_by_id(params[:id])
+
+    if @property.update(property_params)
+      redirect_to @property
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @property = Property.find_by_id(params[:id])
+  end
+
+  def destroy
+    @property = Property.find_by_id(params[:id])
+    
+    @property.destroy
+
+    redirect_to properties_path
   end
 
   private
--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,11 @@
 class Property < ApplicationRecord
   has_many :leases
   has_many :property_managers, through: :leases
+
+  validates :address, presence: true, uniqueness: true
+  validates :unit_number, uniqueness: true
+  validates :monthly_cost, presence: true
+  validates :num_bedrooms, presence: true
+  validates :num_bathroom, presence: true
+  validates :square_footage, presence: true
 end
