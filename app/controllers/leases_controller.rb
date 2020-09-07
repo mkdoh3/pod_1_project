cba5ef42b4233261b89bf4cf53d3768fd075ba2c
Dice-K39 <daisukehashimoto39@gmail.com>
2020-09-07 01:35:01 -0400

    Code Lease views.

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,7 +1,51 @@
 class LeasesController < ApplicationController
+    def index
+        @leases = Lease.all
+    end
+    
     def new 
         @lease = Lease.new 
     end
 
-    
+    def create
+        @lease = Lease .new(lease_params)
+
+        if @lease.save
+            redirect_to lease_path(@lease)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @lease = Lease.find_by_id(params[:id])
+    end
+
+    def update
+        @lease = Lease.find_by_id(params[:id])
+
+        if @lease.update(lease_params)
+            redirect_to @lease
+        else
+            render :edit
+        end
+    end
+
+    def show
+        @lease = Lease.find_by_id(params[:id])
+    end
+
+    def delete
+        @lease = Lease.find_by_id(params[:id])
+
+        @lease.destroy
+
+        redirect_to leases_path
+    end
+
+    private
+
+    def lease_params
+        params.require(:lease).permit(:start_date, :contract_length, :num_tenets)
+    end
 end
